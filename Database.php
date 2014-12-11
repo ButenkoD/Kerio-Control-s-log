4d20de70a8d143bea44d0566c59c1cfd08c00c16
Alex Chuev <nepro4tu@mail.ru>
2014-12-11 10:31:20 +0200

    Saving to db is done

--- Database.php
+++ Database.php
@@ -8,10 +8,42 @@
 
 class Database
 {
-    public $path = '1234567';
-
-    public function getPath()
+    private function setConnection()
     {
-        return $this->path;
+        // Create connection
+        $conn = new mysqli(DB_SERVER_NAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        return $conn;
+    }
+
+    public function saveParsedData($data)
+    {
+        $conn = $this->setConnection();
+        $sql = "INSERT INTO log (username, action_type, date_time)
+                VALUES";
+        foreach($data as $record){
+            $sql .= "('"
+                .$record['username']
+                ."', '"
+                .$record['action']
+                ."', STR_TO_DATE('"
+                .$record['datetime']
+                ."', '%d/%b/%Y %T')), ";
+        }
+
+        $sql = substr($sql, 0, strrpos($sql, ','));
+        $sql .= ";";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $conn->close();
     }
 }
--- index.php
+++ index.php
@@ -20,22 +20,9 @@ $log = file_get_contents(LOG_FILE_PATH);
 $parser = new Parser();
 $data = $parser->parseString($log);
 
-// Create connection
-$conn = new mysqli(DB_SERVER_NAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$databaseHandler = new Database();
+$databaseHandler->saveParsedData($data);
 
-$sql = "INSERT INTO log (username, action_type, date_time)
-VALUES ('John', 'logged in', STR_TO_DATE('13/Dec/2012 08:06:26', '%d/%b/%Y %T'))";
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
 
 var_dump($data);
