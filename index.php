505d9c4ba3fbe74211c2953a49f45529842040e4
apedan <alexandr.pedan@gmail.com>
2014-12-12 13:49:47 +0200

    index file was changed

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apedan
+ * Date: 12.12.14
+ * Time: 13:12
+ */
+?>
+<html>
+<head>
+</head>
+<body>
+<form>
+    <label>Start date</label>
+    <input type="text" id="start-date" name="date" value="2014-12-05">
+    <button onclick="makeRequest('showAll'); return false">Show log records</button>
+    <button onclick="makeRequest('clearDB'); return false;">Clear Database</button>
+    <button onclick="makeRequest('parseLog'); return false;">Parse Kerio log file</button>
+</form>
+<br>
+<div id="txtHint"><b>Required info will be listed here.</b></div>
+<script>
+    function makeRequest(action) {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+            }
+        }
+        var start_date = document.getElementById("start-date").value;
+        xmlhttp.open("GET", "front-controller.php?request_action=" + action + "&start_date=" + start_date, true);
+        xmlhttp.send();
+    }
+</script>
+</body>
+</html>
