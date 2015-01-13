3e4ad8e1f18b0a370e73ca45da22bdab96f106a1
idrummer83 <vodaadminwater@gmail.com>
2015-01-13 15:50:36 +0200

    buton view changed

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ return [
         'dbName' => 'kerio_control',
         'serverName' => 'localhost',
         'username' => 'root',
-        'password' => 'mysql'
+        'password' => ''
     ],
     'log_file_path' => __DIR__ . '/../data/host.log',
     'log_file_tree_dir' => __DIR__ . '/../data/logs',
