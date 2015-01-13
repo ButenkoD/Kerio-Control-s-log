b9991331e0100aee0e2218a6036578e712e5bb3e
idrummer83 <vodaadminwater@gmail.com>
2015-01-13 14:51:27 +0200

    fixes after merge

--- views/main.php
+++ views/main.php
@@ -4,7 +4,7 @@
     <script type="application/javascript" src="../static/js/jquery-ui-1.11.2.custom/jquery-ui.min.js"></script>
     <script type="application/javascript" src="../static/js/jquery.dataTables.min.js"></script>
     <script type="application/javascript" src="../static/bootstrap3/dist/js/bootstrap.min.js"></script>
-    <!--    <script type="application/javascript" src="../static/js/preLoader.js"></script>-->
+<!--    <script type="application/javascript" src="../static/js/preLoader.js"></script>-->
     <link rel="stylesheet" href="../static/js/jquery-ui-1.11.2.custom/jquery-ui.min.css">
     <link rel="stylesheet" href="../static/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="../static/css/jquery.dataTables_themeroller.css">
@@ -19,10 +19,9 @@
             <label>Start date</label>
             <input type="text" id="start-date" name="date" value="2014-12-05">
             <!--onclick="makeRequest(event, 'showAll');-->
-            <!--    <button class="button btn btn-default" onclick="makeRequest(event, 'showAll');">Show log records</button>-->
+    <!--    <button class="button btn btn-default" onclick="makeRequest(event, 'showAll');">Show log records</button>-->
             <button class="button btn btn-warning" onclick="makeRequest(event,'clearDB');">Clear Database</button>
-            <button class="button btn btn-default" onclick="makeRequest(event,'parseLog');">Parse Kerio log file
-            </button>
+            <button class="button btn btn-default" onclick="makeRequest(event,'parseLog');">Parse Kerio log file</button>
             <button class="button btn btn-default" onclick="makeRequest(event, 'showAll');">Show log records</button>
             <input type="radio" name="log-source" value="file" checked/> Use single file
             <input type="radio" name="log-source" value="dir"/> Use directory
@@ -33,7 +32,7 @@
 <div id="txtHint" class="container"><b>Required info will be listed here.</b></div>
 <script>
     function makeRequest(event, action) {
-        $("#loader").css("display", "block");
+        $("#loader").css("display","block");
         event.preventDefault();
 
         if (window.XMLHttpRequest) {
@@ -57,13 +56,14 @@
                     $("#dataTable").addClass("table table-hover table-bordered");
                 }
             }
+            $("#loader").css("display","none");
         }
-        $("#loader").css("display", "none");
+//        $("#loader").css("display", "none");
+
         var start_date = document.getElementById("start-date").value;
         xmlhttp.open("GET", "index.php?request_action=" + action + "&log-source=" + $(":radio:checked").val() + "&start_date=" + start_date, true);
         xmlhttp.send();
     }
-
     $(document).ready(function () {
         $('.button').button();
         $(':input').addClass("ui-widget ui-widget-content ui-corner-all");
