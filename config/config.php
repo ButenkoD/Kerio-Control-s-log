e630f3ffb000bf13a2b3f705f844ec5af0a3d833
yurcka <DT91B7vM>
2015-01-13 15:51:40 +0200

    fix config

--- config/config.php
+++ config/config.php
@@ -5,9 +5,9 @@ return [
         'dbName' => 'kerio_control',
         'serverName' => 'localhost',
         'username' => 'root',
-        'password' => 'mysql'
+        'password' => 'root'
     ],
-    'log_file_path' => __DIR__ . '/data/host.log',
-    'log_file_tree_dir' => __DIR__ . '/data/logs',
+    'log_file_path' => 'http://timereports.andersenlab.com/Kerio-Control-s-log/data/host.log',
+    'log_file_tree_dir' => 'http://timereports.andersenlab.com/Kerio-Control-s-log/data/',
     'showDayNumber' => 5,
 ];
